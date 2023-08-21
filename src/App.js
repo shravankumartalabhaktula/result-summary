1ccc821a738fa6320d50c0f15a1cf761ffb2070f
Shravan Kumar T <shravankumartalabhaktula@gmail.com>
2023-08-21 23:03:55 +0530

    added structure and basic styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import './app.css';
+
 function App() {
-  return <div>Hello There!</div>;
+  return (
+    <>
+      <div className="center main">
+        <div className="result">
+          <div className="result-inner">
+            <div>Your Result</div>
+            <div>76 of 100</div>
+            <div>Great</div>
+            <div>
+              You scored higher than 65% of the people who have taken these
+              tests.
+            </div>
+          </div>
+        </div>
+        <div className="summary">
+          <div>Summary</div>
+          <div>Reaction 80 / 100</div>
+          <div>Memory 92 / 100</div>
+          <div>Verbal 61 / 100</div>
+          <div>Visual 72 / 100 </div>
+          <div>Continue</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
